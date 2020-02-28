2f062a8d3504e164459d1a0ada9b499e092317a6
atmusick <a0musick@gmail.com>
2020-02-27 20:59:07 -0500

    InventorySystem update
    added pickup and drop functions, also returns inv as array

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -10,12 +10,56 @@ public class InventorySystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //instantiate list
+        inv = new List<Items>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool HaveItemName (string itemName)
     {
-        
+        foreach (Items item in inv)
+        {
+            if (itemName == item.itemName)
+            {
+                return true;
+            }
+        }
+        return false;
     }
+
+
+    public void Pickup (Items item)
+    {
+        item.InventorySystem = this;
+        item.onPickup();
+
+        if(item.addToInventory)
+        {
+            if(!inv.Contains(item))
+            {
+                inv.Add(item);
+                {
+                    else
+                    {
+                        Debug.Log ("NO");
+                    }
+                }
+            }
+        }
+    }
+
+    public void Drop (Items item)
+    {
+        if(inv.Contains(item))
+        {
+            inv.Remove(item);
+            item.OnDrop();
+        }
+    }
+
+    public Items[] GetInv()
+        {
+            return inv.ToArray();
+        }
+    
+   
 }
