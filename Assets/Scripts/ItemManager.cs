e9dcf301a9292491dfe6a70ec7f9680970b63552
atmusick <a0musick@gmail.com>
2020-02-27 21:48:42 -0500

    commented itemmanager

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-
+    //should be applied to all item types in play
     public string nameofItem;
     public bool addItem = true;
 
+    //allows reference to item inventory
     public InventorySystem inventorySystem;
 
 
+    //allows override from children if necessary
+    //toggles game object off when picked up
     public virtual void onPickup()
     {
         gameObject.SetActive(false);
     }
-
+    //toggles on when dropped
     public virtual void onDrop()
     {
-        gameObject.SetActive(true);
+        gameObject.SetActive(true); 
     }
 }
